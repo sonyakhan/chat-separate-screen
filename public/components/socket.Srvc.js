6deaf707f8c2be4e7bd430067f7de6cab37d01e5
Sonya Khan <sonya.khan.cs@gmail.com>
2017-05-19 21:14:35 -0400

    connect the users

--- public/components/socket.Srvc.js
+++ public/components/socket.Srvc.js
@@ -8,7 +8,32 @@
   socket.$inject = ['$rootScope'];
 
   function socket($rootScope) {
-    return {}
+    var socket = io.connect();
     // Socket 'on' and 'emit' methods here
+    return {
+      on: on,
+      emit: emit
+    };
+
+    function on(eventName, callback) {
+      socket.on(eventName, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+          callback.apply(socket, args);
+        });
+      });
+    }
+
+    function emit(eventName, data, callback) {
+      socket.emit(eventName, data, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+
   };
 })();
